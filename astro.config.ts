ec9864c38907c42e05c0d7b83ec887b750f4bfab
Your Name <you@example.com>
2025-07-24 16:32:30 -0700

    adding base

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://annie-sommer.github.io',
+  base: '/annie-sommer',
   outDir: 'dist'
 });
 import mdx from '@astrojs/mdx'
